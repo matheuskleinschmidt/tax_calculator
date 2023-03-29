2abff92e2c3c3b1a02fcb9cb9ad8625527077a78
MATHEUS ECKEL KLEINSCHMIDT <matheus.kleinschmidt@catolicasc.edu.br>
2023-03-29 17:05:20 -0300

    Inicial

---
+++ controllers/youtube-chapters-controller.ts
@@ -0,0 +1,12 @@
+import { IChapter, getChaptersData } from "../models/youtube-chapters-model.ts";
+
+const getChapters = async (
+  { params, response }: { params: { id: string }; response: any },
+) => {
+  let chapters = await getChaptersData(params.id);
+  chapters = [{tese:"teste"}]
+  response.status = 200;
+  response.body = chapters;
+};
+
+export { getChapters };
