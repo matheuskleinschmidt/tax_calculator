a22d29afa2d1329be87768e95c617a27ea44bb0a
MATHEUS ECKEL KLEINSCHMIDT <matheus.kleinschmidt@catolicasc.edu.br>
2023-03-30 15:02:35 -0300

    inicial

--- app.ts
+++ app.ts
@@ -1,10 +1,7 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
-import { config } from "https://deno.land/x/dotenv/mod.ts";
 import router from "./routes/routes.ts";
 import { oakCors } from "https://deno.land/x/cors/mod.ts";
 
-const HOST = config().HOST ?? "127.0.0.1";
-const PORT = config().PORT ?? 8080;
 
 const app = new Application();
 
@@ -12,5 +9,5 @@ app.use(oakCors());
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-console.log(`Deno is running: ${HOST}:${PORT}`);
-await app.listen(`${HOST}:${PORT}`);
+console.log();
+await app.listen();
