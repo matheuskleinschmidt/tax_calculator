0715957894edef11d929a5d44af005e00437ab09
Matheus Eckel Kleinschmidt <107818399+matheuskleinschmidt@users.noreply.github.com>
2023-04-05 17:17:59 +0000

    cara , nem eu sei o que fiz aqui

--- controllers/youtube-chapters-controller.ts
+++ controllers/youtube-chapters-controller.ts
@@ -1,5 +1,9 @@
-export const teste = async ({ response }) => {
+import { BodyJson } from "https://deno.land/x/oak/mod.ts";
+
+ const teste =  ({ response }: any) => {
   let chapters = [{ tese: "teste" }]
   response.status = 200;
   response.body = chapters;
 };
+
+export default {teste}
--- routes/routes.ts
+++ routes/routes.ts
@@ -1,8 +1,28 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
-import { teste } from "../controllers/youtube-chapters-controller.ts";
+import  teste  from "../controllers/youtube-chapters-controller.ts";
+import { PrismaClient } from "../generated/client/deno/edge.ts";
+import { config } from "https://deno.land/std@0.163.0/dotenv/mod.ts";
+
+const envVars = await config();
+
+const prisma = new PrismaClient({
+    datasources: {
+      db: {
+        url: envVars.DATABASE_URL,
+      },
+    },
+  });
 
 const router = new Router();
 
-router.get("/", teste);
+router.get("/teste", teste.teste);
+router.get("/", (context) => {
+    context.response.body = "Welcome to the Dinosaur API!";
+  })
+  .get("/dinosaur", async (context) => {
+    // Get all dinosaurs.
+    const dinosaurs = await prisma.dinosaur.findMany();
+    context.response.body = dinosaurs;
+  })
 
 export default router;
