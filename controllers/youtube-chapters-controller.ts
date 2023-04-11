3c49073f60bde96a173e82b4c81e79c6cf2234c3
Matheus Eckel Kleinschmidt <107818399+matheuskleinschmidt@users.noreply.github.com>
2023-04-11 12:54:17 +0000

    Opa

--- controllers/youtube-chapters-controller.ts
+++ controllers/youtube-chapters-controller.ts
@@ -1,7 +1,80 @@
 import { BodyJson } from "https://deno.land/x/oak/mod.ts";
 
  const teste =  ({ response }: any) => {
-  let chapters = [{ tese: "teste" }]
+  let chapters = [
+    {
+      rangeIndex: 1,
+      rBT12: 180_000,
+      nominalRate: 4,
+      deduction: 0,
+      IRPJ: 5.5,
+      CSLL: 3.5,
+      COFINS: 12.74,
+      PIS: 2.76,
+      CPP: 41.5,
+      ICMS: 34
+    },
+    {
+      rangeIndex: 2,
+      rBT12: 360_000,
+      nominalRate: 7.3,
+      deduction: 5_940,
+      IRPJ: 5.5,
+      CSLL: 3.5,
+      COFINS: 12.74,
+      PIS: 2.76,
+      CPP: 41.5,
+      ICMS: 34
+    },
+    {
+      rangeIndex: 3,
+      rBT12: 720_000,
+      nominalRate: 9.5,
+      deduction: 13_860,
+      IRPJ: 5.5,
+      CSLL: 3.5,
+      COFINS: 12.74,
+      PIS: 2.76,
+      CPP: 42,
+      ICMS: 33.5
+    },
+    {
+      rangeIndex: 4,
+      rBT12: 1_800_000,
+      nominalRate: 10.7,
+      deduction: 22_500,
+      IRPJ: 5.5,
+      CSLL: 3.5,
+      COFINS: 12.74,
+      PIS: 2.76,
+      CPP: 42,
+      ICMS: 33.5
+    },
+    {
+      rangeIndex: 5,
+      rBT12: 3_600_000,
+      nominalRate: 14.3,
+      deduction: 87_300,
+      IRPJ: 5.5,
+      CSLL: 3.5,
+      COFINS: 12.74,
+      PIS: 2.76,
+      CPP: 42,
+      ICMS: 33.5
+    },
+    {
+      rangeIndex: 6,
+      rBT12: 4_800_000,
+      nominalRate: 19,
+      deduction: 37_800,
+      IRPJ: 13.5,
+      CSLL: 10,
+      COFINS: 28.27,
+      PIS: 6.13,
+      CPP: 42.1,
+      ICMS: 0
+    }
+  ]
   response.status = 200;
   response.body = chapters;
 };
