14b31a1d56613f073fe36519453ea1c118d4e150
Matheus Eckel Kleinschmidt <matheus.kleinschmidt@catolicasc.edu.br>
2023-05-17 22:27:51 -0300

    colocando porta padrão

--- app.ts
+++ app.ts
@@ -7,6 +7,7 @@ const app = new Application();
 app.use(oakCors());
 app.use(router.routes());
 app.use(router.allowedMethods());
+const port = 8000;
 
 console.log("rodando na porta em alguma porta");
-await app.listen();
+await app.listen({ port: port });
