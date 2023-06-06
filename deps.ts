b18db1c7f799e7740498d4e5c5854215c561bf01
Matheus Eckel Kleinschmidt <107818399+matheuskleinschmidt@users.noreply.github.com>
2023-06-06 12:00:14 +0000

    fix

--- deps.ts
+++ deps.ts
@@ -3,3 +3,4 @@ export { Application, Router, BodyJson } from "https://deno.land/x/oak@v12.1.0/m
 export { oakCors } from "https://deno.land/x/cors/mod.ts";
 export { PrismaClient } from "../generated/client/deno/edge.ts";
 export { config } from "https://deno.land/std@0.163.0/dotenv/mod.ts";
+export { PrismaClient } from "./generated/client/deno/edge.ts";
