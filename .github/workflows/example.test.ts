d79cab89a874ac44557272071fffda36123b1ae1
Matheus Eckel Kleinschmidt <107818399+matheuskleinschmidt@users.noreply.github.com>
2023-06-07 00:17:03 +0000

    fix

--- .github/workflows/example.test.ts
+++ .github/workflows/example.test.ts
@@ -1,13 +1,6 @@
-import { assertEquals, assertNotEquals } from "https://deno.land/std/testing/asserts.ts";
-import { superdeno } from "https://deno.land/x/superdeno/mod.ts";
-import { app } from "./app.ts";
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 
-Deno.test("GET /api/users", async () => {
-  const request = await superdeno(app)
-    .get("/api/users")
-    .expect(200)
-    .expect("Content-Type", /json/);
-
-  assertEquals(request.body.length, 2);
-  assertEquals(request.body[0].name, "John");
+Deno.test("simple addition", () => {
+  const result = 1 + 1;
+  assertEquals(result, 2);
 });
